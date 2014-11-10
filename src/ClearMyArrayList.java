cf990cd2ac20ccf8691ceff8108fbd389f244907
vijaypasunoori <vijay.pasunoori@valuelabs.net>
2014-11-10 14:57:32 +0530

    Update ClearMyArrayList.java
    my first commit

--- src/ClearMyArrayList.java
+++ src/ClearMyArrayList.java
@@ -13,7 +13,8 @@ public class ClearMyArrayList {
         System.out.println("Actual ArrayList:"+arrl);
         arrl.clear();
        // System.out.println("After clear ArrayList:"+arrl);
-       // System.out.println("last line fo method");
+        System.out.println("last line fo method");
+         System.out.println("Last but one line fo method");
     
     }
 }
