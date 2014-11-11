a71b18ed6fdf89097b94abf2bb1f38f6d975f5c1
sknunna <sknunna@A4112E1D.corp.ads.valuelabs.net>
2014-11-11 14:51:56 +0530

    removed line here.
    Change-Id: I8c93709a6218c941ca00b6fa4f800cc0b8eab87c

--- src/ClearMyArrayList.java
+++ src/ClearMyArrayList.java
@@ -18,10 +18,9 @@ public class ClearMyArrayList {
         //System.out.println("Last but one line fo method");
          //System.out.println("Last but one line fo method");
          //System.out.println("Last but one line fo method");
-         // suneel commit here
-        // suneel added new line here
          
-
+        // suneel added new line here
+        
         // System.out.println("Last but one line fo method");
     
     }
