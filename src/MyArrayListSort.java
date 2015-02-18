d2f688c9963e3addc9c665986764c1a83e0bdfc1
rajyalakshmi <rajyalakshmi@a7079q1d.corp.ads.valuelabs.net>
2015-02-18 11:14:49 +0530

    MyArratListSort commited

--- src/MyArrayListSort.java
+++ src/MyArrayListSort.java
@@ -11,7 +11,7 @@ public class MyArrayListSort {
         list.add(new Empl("Ram",3000));
         list.add(new Empl("John",6000));
         list.add(new Empl("Crish",2000));
-        list.add(new Empl("Tom",2400));
+        //list.add(new Empl("Tom",2400));
         Collections.sort(list,new MySalaryComp());
         System.out.println("Sorted list entries: ");
         for(Empl e:list){
