085ef2f7436b1956a9f2b8a6c02c99f83e0bc2b5
chiranjeevi <chiranjeevi.kollipara@valuelabs.net>
2015-02-18 13:44:07 +0530

    Change commited

--- src/ClearMyArrayList.java
+++ src/ClearMyArrayList.java
@@ -6,10 +6,10 @@ public class ClearMyArrayList {
     public static void main(String a[]){
         ArrayList<String> arrl = new ArrayList<String>();
         adding elements to the end
-        /*arrl.add("test");
+        arrl.add("test");
         arrl.add("revanth");
         arrl.add("changed");
-        arrl.add("Random");*/
+        arrl.add("Random");
         System.out.println("Actual ArrayList:"+arrl);
         arrl.clear();
 
